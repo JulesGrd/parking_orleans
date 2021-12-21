84e495076fd695ae27b431e586e5f0ce8502d743
jules <julesgirard@laposte.net>
2021-12-21 02:31:00 +0100

    fix background task

--- clock.py
+++ clock.py
@@ -1,11 +1,15 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+import pytz
 
 sched = BlockingScheduler()
 
 from datetime import datetime
 
+# set timezone
+pytz.timezone('Europe/Paris')
 
-@sched.scheduled_job('interval', minutes=0.5)
+
+@sched.scheduled_job('interval', minutes=10)
 def timed_job():
     print('This job is run every three minutes.')
 
